dbeacd5395ebe40e29daf1a5527e0fbcc47c1961
rminor2 <146909693+rminor2@users.noreply.github.com>
2024-02-03 23:15:51 -0700

    updates

--- examples/ch02/fig02_01.c
+++ examples/ch02/fig02_01.c
@@ -2,6 +2,9 @@
 // A first program in C.
 #include <stdio.h>
 
+double calculateArea(double, double);
+double calculateAverage(double, double);
+
 // function main begins program execution 
 int main(void) {
    double length, width, area, result, average;
@@ -31,8 +34,8 @@ int main(void) {
       }
    } while (result != 1);
 
-   area = length * width;
-   average = (length + width) / 2;
+   area = calculateArea(length, width);
+   average = calculateAverage(length, width);
 
    printf("Length: %0.1f Width: %0.1f and Area is %0.1f \n", length, width, area);
    printf("The Average of the Lenght and Width is: %0.1f \n", average);
@@ -40,7 +43,13 @@ int main(void) {
 return 0;   
 } // end function main 
 
+double calculateArea(double num1, double num2) {
+   return num1 * num2;
+}
 
+double calculateAverage(double num1, double num2) {
+   return (num1 + num2) / 2;
+}
 
 /**************************************************************************
  * (C) Copyright 1992-2021 by Deitel & Associates, Inc. and               *
