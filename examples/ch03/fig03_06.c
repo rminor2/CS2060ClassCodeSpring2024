5b5a64904b484d388b90cb26178bbe4fcb1c6745
rminor2 <146909693+rminor2@users.noreply.github.com>
2024-02-06 19:56:22 -0700

    update

--- examples/ch03/fig03_06.c
+++ examples/ch03/fig03_06.c
@@ -8,14 +8,20 @@ int main(void) {
    int passes = 0; 
    int failures = 0; 
    int student = 1;  
+   int result = 0; // one exam result 
 
    // process 10 students using counter-controlled loop 
    while (student <= 10) {
       // prompt user for input and obtain value from user 
-      printf("%s", "Enter result (1=pass,2=fail): ");
-      int result = 0; // one exam result 
-      scanf("%d", &result);
+     do {
 
+         printf("%s", "Enter result (1=pass,2=fail): ");
+         scanf("%d", &result);
+
+         // clear the input buffer to avoid infinite loops   
+         while (getchar() != '\n');
+         
+     } while (result != 1 && result != 2);
       // if result 1, increment passes 
       if (result == 1) {     
          passes = passes + 1;
